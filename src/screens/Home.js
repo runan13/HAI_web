10e5a1ef13e67be4fc3ed046a0c4ae99733130b6
runan13 <65391973+runan13@users.noreply.github.com>
2021-02-26 14:13:43 +0900

    Login

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,10 @@
-import { isLoggedInVar } from "../apollo";
+import { logUserOut } from "../apollo";
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <button onClick={() => isLoggedInVar(false)}>Log out Now!</button>
+      <h1>Welcome to Home</h1>
+      <button onClick={() => logUserOut()}>Log out Now!</button>
     </div>
   );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,3 +1,4 @@
+import { gql, useMutation } from "@apollo/client";
 import {
   faFacebookSquare,
   faInstagram,
@@ -5,6 +6,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { logUserIn } from "../apollo";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
@@ -23,13 +25,61 @@ const FacebookLogin = styled.div`
   }
 `;
 
+const LOGIN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      token
+      error
+    }
+  }
+`;
+
 function Login() {
-  const { register, handleSubmit, errors, formState } = useForm({
+  const {
+    register,
+    handleSubmit,
+    errors,
+    formState,
+    getValues,
+    setError,
+    clearErrors,
+  } = useForm({
     mode: "onChange",
   });
-  const onSubmitValid = (data) => {
-    console.log(data);
+
+  const onCompleted = (data) => {
+    const {
+      login: { ok, error, token },
+    } = data;
+    if (!ok) {
+      setError("result", {
+        message: error,
+      });
+    }
+    if (token) {
+      logUserIn(token);
+    }
   };
+
+  const [login, { loading }] = useMutation(LOGIN_MUTATION, {
+    onCompleted,
+  });
+
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    const { username, password } = getValues();
+    login({
+      variables: { username, password },
+    });
+  };
+
+  const clearLoginError = () => {
+    clearErrors("result");
+  };
+
   return (
     <AuthLayout>
       <PageTitle title="Login" />
@@ -46,6 +96,7 @@ function Login() {
                 message: "Short Username, more than 5 chars",
               },
             })}
+            onChange={clearLoginError}
             name="username"
             type="text"
             placeholder="Username"
@@ -56,13 +107,19 @@ function Login() {
             ref={register({
               required: "Password is Required",
             })}
+            onChange={clearLoginError}
             name="password"
             type="password"
             placeholder="Password"
             hasError={Boolean(errors.password?.message)}
           />
           <FormError message={errors.password?.message} />
-          <Button type="submit" value="Log in" disabled={!formState.isValid} />
+          <Button
+            type="submit"
+            value={loading ? "Loading.." : "Log in"}
+            disabled={!formState.isValid || loading}
+          />
+          <FormError message={errors.result?.message} />
         </form>
         <Separator />
         <FacebookLogin>
