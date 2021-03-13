e28215317c189f3ff152e843953fb65c8c3a32d2
runan13 <65391973+runan13@users.noreply.github.com>
2021-03-13 15:48:22 +0900

    SpO2 Screen

--- src/screens/SpO2.js
+++ src/screens/SpO2.js
@@ -1,6 +1,10 @@
 import { gql, useMutation } from "@apollo/client";
+import { faHeartbeat, faRunning } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useParams } from "react-router";
-import { BaseBox } from "../components/shared";
+import styled from "styled-components";
+import Button from "../components/auth/Button";
+import { BaseBox, DivTitle } from "../components/shared";
 
 const UPLOADSPO2_MUTATION = gql`
   mutation uploadSpo2($username: String!) {
@@ -19,9 +23,63 @@ const UPLOADSPO2_MUTATION = gql`
   }
 `;
 
+const SpO2Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const SpO2Btn = styled(Button).attrs({
+  as: "button",
+})`
+  max-width: 300px;
+  height: 50px;
+  cursor: pointer;
+`;
+
+const TopContentContainer = styled.div`
+  display: flex;
+  width: 1000px;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
+
+const TopContent = styled(BaseBox)`
+  margin-right: 30px;
+  height: 100px;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  &:last-child {
+    margin-right: 0px;
+  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const TopContentTitle = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    margin-left: 10px;
+    font-weight: 600;
+    font-size: 16px;
+  }
+`;
+
+const TopContentValue = styled.div`
+  margin-top: 13px;
+  font-size: 24px;
+  font-weight: 500;
+`;
+
 function SpO2() {
   const { username } = useParams();
-  console.log(username);
+  let ok = false;
+  let health = "";
+  let avgSpo2 = 0;
   const onCompleted = (data) => {
     const {
       uploadSpo2: { id },
@@ -29,19 +87,68 @@ function SpO2() {
     if (!id) {
       return;
     } else {
-      console.log(data);
     }
   };
-  const [uploadSpo2, { loading }] = useMutation(UPLOADSPO2_MUTATION, {
+  const [uploadSpo2, { data, loading }] = useMutation(UPLOADSPO2_MUTATION, {
     onCompleted,
     variables: {
       username,
     },
   });
+  console.log(data);
+  if (data !== undefined) {
+    ok = true;
+  }
+  try {
+    avgSpo2 = data?.uploadSpo2?.avgSpo2;
+    if (avgSpo2 < 90) {
+      health = "Bad";
+    } else if (avgSpo2 < 96) {
+      health = "Good";
+    } else if (avgSpo2 >= 96) {
+      health = "Very Good";
+    } else if (avgSpo2 === null || avgSpo2 === "unsigned") {
+      health = "null";
+    }
+  } catch (e) {
+    avgSpo2 = 0;
+    health = "";
+  }
   return (
     <>
-      <button onClick={uploadSpo2}>test</button>
-      {loading ? "Loading.." : "Done !"}
+      <SpO2Container>
+        <SpO2Btn onClick={uploadSpo2}>
+          {loading ? "측정중..." : "SpO2 측정"}
+        </SpO2Btn>
+        <DivTitle>{loading ? "Loading..." : ""}</DivTitle>
+        {ok ? (
+          <>
+            <DivTitle>측정 완료</DivTitle>
+            <TopContentContainer>
+              <TopContent>
+                <TopContentTitle>
+                  <FontAwesomeIcon icon={faHeartbeat} size="2x" color="red" />
+                  <span>측정 SpO2 평균</span>
+                </TopContentTitle>
+                <TopContentValue>{data?.uploadSpo2?.avgSpo2}%</TopContentValue>
+              </TopContent>
+              <TopContent>
+                <TopContentTitle>
+                  <FontAwesomeIcon icon={faRunning} size="2x" color="blue" />
+                  <span>건강 상태</span>
+                </TopContentTitle>
+                <TopContentValue>{health}</TopContentValue>
+              </TopContent>
+              <TopContent>
+                <TopContentTitle>혈압</TopContentTitle>
+              </TopContent>
+              <TopContent>
+                <TopContentTitle>혈압상태</TopContentTitle>
+              </TopContent>
+            </TopContentContainer>
+          </>
+        ) : null}
+      </SpO2Container>
     </>
   );
 }
